c937c91ab118785d3da1b579f0a649ab476a7a01
Hugh Cheng <hcheng1@hwemail.com>
2024-09-07 16:06:34 -0700

    Specify and Use FileWriter
    Add hidden and non-hidden files using chosen writing method; create .gitignore file

--- MyFileWriter.java
+++ MyFileWriter.java
@@ -1,49 +1,31 @@
 import java.io.*;
-import java.nio.file.*;
-import java.nio.charset.StandardCharsets;
 
 public class MyFileWriter {
     public static void main(String[] args) {
-        String data = "Greetings, Universe!";
-        String fileName1 = "example1.txt";
-        String fileName2 = "example2.txt";
-        String fileName3 = "example3.txt";
-        String fileName4 = "example4.txt";
-        String fileName5 = "example5.txt";
+        String password = "helloworld1234";
+        String data = "glorb";
+        String hiddenFileName = ".supersecretexample.txt";
+        String hiddenDirectoryName = ".confidential";
+        String extraHiddenFileName = ".classified.dat";
 
-        // 1. Using FileWriter
-        try (FileWriter writer = new FileWriter(fileName1)) {
-            writer.write(data);
+        // BufferedOutputStream
+        try (BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(
+                new FileOutputStream(hiddenFileName))) {
+            bufferedOutputStream.write(password.getBytes());
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        // 2. Using BufferedWriter
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName2))) {
-            bufferedWriter.write(data);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        File hiddenDirectory = new File(hiddenDirectoryName);
+        if (!hiddenDirectory.exists())
+            hiddenDirectory.mkdir();
 
-        // 3. Using FileOutputStream
-        try (FileOutputStream outputStream = new FileOutputStream(fileName3)) {
-            outputStream.write(data.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // 4. Using BufferedOutputStream
-        try (BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(fileName4))) {
+        try (BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(
+                new FileOutputStream(new File(hiddenDirectory, extraHiddenFileName)))) {
             bufferedOutputStream.write(data.getBytes());
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        // 5. Using Files (java.nio.file)
-        try {
-            Files.write(Paths.get(fileName5), data.getBytes(StandardCharsets.UTF_8));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
